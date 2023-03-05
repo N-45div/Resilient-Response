91466820e092e5c824529e8e6a373c47edb75542
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-05 19:13:14 +0530

    Update RightNav.jsx

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom';
 const RightNav = ({ open }) => {
   return (
     <>
-      <div className=' p-3'>
-        <Link to="/" className='text-decoration-none text-black m-4'>Home</Link>
-        <Link to="/Signup" className='text-decoration-none text-black m-4'>Community support</Link>
-        <Link to="/ImageClassifier" className='text-decoration-none text-black m-4'>Image classify</Link>
-        <Link to="/Notify" className='text-decoration-none text-black m-4'>Disaster Alerts</Link>
-        <Link to="/Safety" className='text-decoration-none text-black m-4'>Safety tips</Link>
-        <Link to="/Donation" className='text-decoration-none text-black m-4'>Donate the needy</Link>
+      <div className='p-3'>
+        <Link to="/" className='text-decoration-none text-black m-2 m-sm-4'>Home</Link>
+        <Link to="/Signup" className='text-decoration-none text-black m-2 m-sm-4'>Community support</Link>
+        <Link to="/ImageClassifier" className='text-decoration-none text-black m-2 m-sm-4'>Image classify</Link>
+        <Link to="/Notify" className='text-decoration-none text-black m-2 m-sm-4'>Disaster Alerts</Link>
+        <Link to="/Safety" className='text-decoration-none text-black m-2 m-sm-4'>Safety tips</Link>
+        <Link to="/Donation" className='text-decoration-none text-black m-2 m-sm-4'>Donate the needy</Link>
       </div>
     </>
   )
 }
 
 export default RightNav;
+
 
 
 
