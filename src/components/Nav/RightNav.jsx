d2f9071832af23657d332ef42879b5b96bdb195b
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-07 20:40:40 +0530

    Update RightNav.jsx

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -1,78 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-const Ul = styled.ul`
-  list-style: none;
-  display: flex;
-  z-index: 1;
-  flex-flow: row nowrap;
-  padding: 15px 5px;
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-  @media (max-width: 768px) {
-    flex-flow: column nowrap;
-    background-color: #ffffff;
-    position: fixed;
-    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
-    top: 0;
-    right: 0;
-    height: 60vh;
-    width: 220px;
-    padding: 50px 5px;
-    transition: transform 0.3s ease-in-out;
-  }
-  li {
-    margin-bottom: 1rem;
-  }
-`;
 const RightNav = ({ open }) => {
   return (
-    <Ul open={open}>
-      <li>
-        <Link to="/" className="text-decoration-none text-black m-4">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/community" className="text-decoration-none text-black m-4">
-          Community support
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/ImageClassifier"
-          className="text-decoration-none text-black m-4"
-        >
-          Image classify
-        </Link>
-      </li>
-      <li>
-        <Link to="/Notify" className="text-decoration-none text-black m-4">
-          Disaster Alerts
-        </Link>
-      </li>
-      <li>
-        <Link to="/Notify" className="text-decoration-none text-black m-4">
-          Disaster Alerts
-        </Link>
-      </li>
-      <li>
-        <Link to="/MarkerMap" className="text-decoration-none text-black m-4">
-          Emergency Shelters
-        </Link>
-      </li>
+    <>
+      <div className='p-3'>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Menu
+          </Dropdown.Toggle>
 
-      <li>
-        <Link to="/Safety" className="text-decoration-none text-black m-4">
-          Safety tips
-        </Link>
-      </li>
-      <li>
-        <Link to="/Donation" className="text-decoration-none text-black m-4">
-          Donate the needy
-        </Link>
-      </li>
-    </Ul>
-  );
-};
+          <Dropdown.Menu>
+            <Dropdown.Item><Link to="/" className='text-decoration-none text-black'>Home</Link></Dropdown.Item>
+            <Dropdown.Item><Link to="/community" className='text-decoration-none text-black'>Community support</Link></Dropdown.Item>
+            <Dropdown.Item><Link to="/ImageClassifier" className='text-decoration-none text-black'>Image classify</Link></Dropdown.Item>
+            <Dropdown.Item><Link to="/Notify" className='text-decoration-none text-black'>Disaster Alerts</Link></Dropdown.Item>
+            <Dropdown.Item><Link to="/Safety" className='text-decoration-none text-black'>Safety tips</Link></Dropdown.Item>
+            <Dropdown.Item><Link to="/MarkerMap" className='text-decoration-none text-black'>Emergency shelters</Link></Dropdown.Item>
+            <Dropdown.Item><Link to="/Donation" className='text-decoration-none text-black'>Donate the needy</Link></Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </>
+  )
+}
 
 export default RightNav;
