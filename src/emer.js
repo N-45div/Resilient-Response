2e4abecf20ffead251f0a2d1f015ed9b7809145e
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-08 02:04:33 +0530

    Update emer.js

--- src/emer.js
+++ src/emer.js
@@ -2,18 +2,12 @@ import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 const emergencyContacts = [
-    { name: 'Police', number: '911' },
     { name: 'Fire Department', number: '911' },
-    { name: 'Ambulance', number: '911' },
-    { name: 'Poison Control', number: '1-800-222-1222' },
-    { name: 'National Suicide Prevention Lifeline', number: '1-800-273-8255' },
     { name: 'FEMA Disaster Assistance', number: '1-800-621-3362' },
     { name: 'American Red Cross', number: '1-800-733-2767' },
     { name: 'National Weather Service', number: '1-301-713-9142' },
-    { name: 'Animal Poison Control Center', number: '1-888-426-4435' },
     { name: 'FEMA (Federal Emergency Management Agency)', number: '1-800-621-3362' },
     { name: 'Red Cross', number: '1-800-733-2767' },
-    { name: 'National Weather Service', number: '1-888-976-7468' },
     { name: 'National Hurricane Center', number: '1-305-229-4470' },
     { name: 'USGS (U.S. Geological Survey) Earthquake Hazards Program', number: '1-888-275-8747' },
     { name: 'National Tsunami Warning Center', number: '1-888-851-9350' }
