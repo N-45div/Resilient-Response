f185ef83afba5d1cd77a1e5fba5b8ec313f6b223
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-07 15:17:18 +0530

    Update Notify.js

--- src/Notify.js
+++ src/Notify.js
@@ -8,16 +8,15 @@ function App() {
     const fetchAlerts = async () => {
       try {
         const response = await fetch(
-          'https://newsapi.org/v2/top-headlines?q=natural%20disaster&language=en&apiKey=020f4dff9f3e4a86be5d43cc11dd5ed2'
-
+          'https://newsapi.org/v2/everything?q=natural%20disaster&apiKey=020f4dff9f3e4a86be5d43cc11dd5ed2'
         );
         const data = await response.json();
         const newAlerts = data.articles.map((article) => ({
-          id: article.publishedAt,
+          id: article.title,
           title: article.title,
           description: article.description,
           url: article.url,
-          image: article.image?.thumbnail?.contentUrl,
+          imageUrl: article.urlToImage,
         }));
         setAlerts(newAlerts);
       } catch (error) {
@@ -32,26 +31,26 @@ function App() {
   useEffect(() => {
     if (alerts.length > 0) {
       const latestAlert = alerts[0];
-      const title = `New disaster news alert: ${latestAlert.title}`;
+      const title = `New disaster alert: ${latestAlert.title}`;
       const options = {
         body: latestAlert.description,
-        icon: latestAlert.image,
+        icon: latestAlert.imageUrl || '/path/to/default-icon.png',
       };
       const notification = new Notification(title, options);
       notification.addEventListener('click', () => {
-        window.open(latestAlert.url);
+        window.open(latestAlert.url, '_blank');
       });
       setTimeout(() => notification.close(), 5000); // Close notification after 5 seconds
     }
   }, [alerts]);
 
   const handleAlertClick = (alert) => {
-    window.open(alert.url);
+    window.open(alert.url, '_blank');
   };
 
   return (
     <div>
-      <h1 className="heading">Real-time Disaster News Alert Notifications</h1>
+      <h1>Disaster News Alert Notifications</h1>
       <div className="alert-container">
         {alerts.map((alert) => (
           <div className="alert-box" key={alert.id} onClick={() => handleAlertClick(alert)}>
@@ -65,5 +64,6 @@ function App() {
 }
 
 export default App;
+
 
 
