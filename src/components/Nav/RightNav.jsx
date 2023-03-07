aaf603fd8b6438824b679a46713ac436cabb5cd1
enpvivek <enpvivek@gmail.com>
2023-03-07 20:11:32 +0530

    added emergency shelters

--- src/components/Nav/RightNav.jsx
+++ src/components/Nav/RightNav.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 const Ul = styled.ul`
   list-style: none;
   display: flex;
@@ -12,34 +12,67 @@ const Ul = styled.ul`
     flex-flow: column nowrap;
     background-color: #ffffff;
     position: fixed;
-    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
     top: 0;
     right: 0;
     height: 60vh;
     width: 220px;
     padding: 50px 5px;
     transition: transform 0.3s ease-in-out;
-
   }
-  li{
+  li {
     margin-bottom: 1rem;
   }
 `;
 const RightNav = ({ open }) => {
   return (
     <Ul open={open}>
-      <li><Link to="/" className='text-decoration-none text-black m-4'>Home</Link></li>
-      <li><Link to="/community" className='text-decoration-none text-black m-4'>Community support</Link></li>
-      <li><Link to="/ImageClassifier" className='text-decoration-none text-black m-4'>Image classify</Link></li>
-      <li><Link to="/Notify" className='text-decoration-none text-black m-4'>Disaster Alerts</Link></li>
-      <li><Link to="/Safety" className='text-decoration-none text-black m-4'>Safety tips</Link></li>
-      <li><Link to="/Donation" className='text-decoration-none text-black m-4'>Donate the needy</Link></li>
-     </Ul>
-  )
-}
+      <li>
+        <Link to="/" className="text-decoration-none text-black m-4">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/community" className="text-decoration-none text-black m-4">
+          Community support
+        </Link>
+      </li>
+      <li>
+        <Link
+          to="/ImageClassifier"
+          className="text-decoration-none text-black m-4"
+        >
+          Image classify
+        </Link>
+      </li>
+      <li>
+        <Link to="/Notify" className="text-decoration-none text-black m-4">
+          Disaster Alerts
+        </Link>
+      </li>
+      <li>
+        <Link to="/Notify" className="text-decoration-none text-black m-4">
+          Disaster Alerts
+        </Link>
+      </li>
+      <li>
+        <Link to="/MarkerMap" className="text-decoration-none text-black m-4">
+          Emergency Shelters
+        </Link>
+      </li>
 
-export default RightNav
+      <li>
+        <Link to="/Safety" className="text-decoration-none text-black m-4">
+          Safety tips
+        </Link>
+      </li>
+      <li>
+        <Link to="/Donation" className="text-decoration-none text-black m-4">
+          Donate the needy
+        </Link>
+      </li>
+    </Ul>
+  );
+};
 
-
-
-
+export default RightNav;
