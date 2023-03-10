e63d181686bd60df57ad00b19cf0c90177a6d422
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-10 16:47:14 +0530

    Add files via upload

---
+++ src/components/LoginForm.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+const projectID = '72d8d715-2e67-475d-8f2f-8b0929b92971';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
+
+    try {
+      const response = await fetch('https://api.chatengine.io/chats', {
+        method: 'GET',
+        headers: authObject,
+      });
+
+      if (response.ok) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+
+        window.location.reload();
+        setError('');
+      } else {
+        setError('Oops, incorrect credentials.');
+      }
+    } catch (err) {
+      console.error(err);
+      setError('An error occurred while logging in.');
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start chatting</span>
+            </button>
+          </div>
+        </form>
+        <h1>{error}</h1>
+      </div>
+    </div>
+
+  );
+};
+
+export default LoginForm;
