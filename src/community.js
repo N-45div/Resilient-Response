393c20dfd400b972e76f2bf770aa30f426f2ccc2
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-12 13:03:20 +0530

    Update community.js

--- src/community.js
+++ src/community.js
@@ -12,7 +12,7 @@ const CommunityPage = () => {
 
   return (
     <ChatEngine
-      height="60vh"
+      height="71vh"
       projectID={projectID}
       userName={localStorage.getItem('username')}
       userSecret={localStorage.getItem('password')}
