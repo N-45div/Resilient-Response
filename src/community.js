dcb9e239d50bc0eabb81baf190bca7cf6907d2ae
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-13 23:21:15 +0530

    Add files via upload

---
+++ src/community.js
@@ -0,0 +1,33 @@
+import { ChatEngine } from 'react-chat-engine';
+
+//import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import Navbar from './components/Nav/Navbar';
+import Footer from './Footer';
+
+import './style.css';
+
+const projectID = '72d8d715-2e67-475d-8f2f-8b0929b92971';
+
+const CommunityPage = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <div className="App">
+      <Navbar/>
+    <ChatEngine
+      height="75vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+    />
+    <Footer/>
+    </div>
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default CommunityPage;
+
+
