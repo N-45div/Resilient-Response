b1d4ec81ad7aa2b70d15e416436a6c6e287b1f04
enpvivek <enpvivek@gmail.com>
2023-03-13 14:13:13 +0530

    integrated signin logout in header

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { auth, provider } from "./config";
+import { signInWithPopup } from "firebase/auth";
 import styled from "styled-components";
 
-const Nav = styled.nav`
-  width: 100%;
-  height: 55px;
-  background-color: #0b3861;
-  color: #ffffff;
-  padding: 0 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+function Navbar() {
+  const Nav = styled.nav`
+    width: 100%;
+    height: 55px;
+    background-color: #0b3861;
+    color: #ffffff;
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-  .logo {
-    font-weight: bold;
-    font-size: 1.2rem;
-    padding: 10px;
-  }
-`;
+    .logo {
+      font-weight: bold;
+      font-size: 1.2rem;
+      padding: 10px;
+    }
+  `;
 
-const Navbar = () => {
+  const [value, setValue] = useState("");
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setValue(data.user.email);
+      localStorage.setItem("email", data.user.email);
+    });
+  };
+  useEffect(() => {
+    setValue(localStorage.getItem("email"));
+  });
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
   return (
     <Nav>
       <div className="logo">Resilient-Response</div>
+      {value ? (
+        <button onClick={logout}> LogOut</button>
+      ) : (
+        <button onClick={handleClick}> LogIn</button>
+      )}
     </Nav>
   );
-};
+}
 
 export default Navbar;
--- src/components/Nav/SignIn.js
+++ src/components/Nav/SignIn.js
@@ -1,27 +1,29 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { auth, provider } from "./config";
 import { signInWithPopup } from "firebase/auth";
-import { Button } from "bootstrap";
 import Home from "../../Home";
 
-function SignIn(){
-    const [value, setValue] = useState('');
-    const handleClick = ()=>{
-        signInWithPopup(auth, provider).then((data)=>{
-            setValue(data.user.email)
-            localStorage.setItem('email', data.user.email)
-        })
-    }
-    useEffect(() => {
-        setValue(localStorage.getItem('email'))
-    })
+function SignIn() {
+  const [value, setValue] = useState("");
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setValue(data.user.email);
+      localStorage.setItem("email", data.user.email);
+    });
+  };
+  useEffect(() => {
+    setValue(localStorage.getItem("email"));
+  });
 
-return (
+  return (
     <div>
-        {value? <Home/> : <Button onClick={handleClick} > SignIn with Google</Button> }
+      {value ? (
+        <Home />
+      ) : (
+        <button onClick={handleClick}> SignIn with Google</button>
+      )}
     </div>
-);
+  );
 }
 
 export default SignIn;
-
