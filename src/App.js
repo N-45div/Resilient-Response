3fed980cabfaae33680b078c681243220d2b1867
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-13 21:30:38 +0530

    Revert "make changes accordingly"

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
 import Notification from "./Notify";
-
+import CommunityPage from "./community";
 import QRGenerator from "./Safety";
 import Donation from "./Donation";
 import MarkerMap from "./MarkerMap";
 import EmergencyContacts from "./emer";
 import SignIn from "./components/Nav/SignIn";
 import HomePage from "./openpg";
-import GroupChat from "./GroupChat";
 
 function App() {
   return (
@@ -22,11 +21,12 @@ function App() {
         <Route path="/Donation" element={<Donation />} />
         <Route path="/MarkerMap" element={<MarkerMap />} />
         <Route path="/emer" element={<EmergencyContacts />} />
+        <Route path="/community" element={<CommunityPage />} />
         <Route path="/SignIn" element={<SignIn />} />
-        <Route path="/GroupChat" element={<GroupChat />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
