6d0cf98273c1baea34b813dfe061d246b496bf92
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-14 20:54:56 +0530

    Update Donation.js

--- src/Donation.js
+++ src/Donation.js
@@ -1,80 +1,131 @@
-import { useState } from 'react';
-import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap';
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { FaBox, FaDonate } from 'react-icons/fa';
+import styled from 'styled-components';
 import Navbar from './components/Nav/Navbar';
 import Footer from './Footer';
+import image from './assets/logo2.jpg';
 
+const MainContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
-function Donation() {
-  const [donationItems, setDonationItems] = useState([
-    { name: 'Food Gift Box', price: 5, quantity: 0 },
-    { name: 'Clothes Gift Box', price: 7, quantity: 0 }
-  ]);
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  background-color: #3c64b1;
+  color: #fff;
+`;
 
-  const [totalCost, setTotalCost] = useState(0);
+const ContentContainer = styled(Container)`
+  padding: 2rem 2rem 4rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
 
-  const handleQuantityChange = (index, event) => {
-    const newQuantity = parseInt(event.target.value);
-    const newDonationItems = [...donationItems];
-    newDonationItems[index].quantity = newQuantity;
-    setDonationItems(newDonationItems);
-  };
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
 
-  const handleDonateNow = () => {
-    const newTotalCost = donationItems.reduce((acc, item) => {
-      return acc + item.price * item.quantity;
-    }, 0);
-    setTotalCost(newTotalCost);
-  };
+const InputField = styled.input`
+  width: 100%;
+  padding: 1rem;
+  margin: 1rem 0;
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+`;
 
+const DonationButton = styled(Button)`
+  background-color: #3c64b1;
+  border: none;
+  &:hover {
+    background-color: #2c4681;
+  }
+`;
+
+const DonateWithoutMoneyContainer = styled.div`
+  margin-top: 1rem;
+  font-size: 0.8rem;
+`;
+
+const GiftBoxContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 3rem;
+`;
+
+const GiftBoxItem = styled.div`
+  width: 300px;
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  text-align: center;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+`;
+
+const GiftBoxImage = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  margin-bottom: 1rem;
+`;
+
+const Donation = () => {
   return (
-    <div style={{ background: '#F5F5F5', color: '#0B3861' }}>
+    <div className="App">
       <Navbar/>
-      <Container className="my-5">
-        <h2 className="text-center mb-4">Delivering hope through your gift: Disaster relief box.</h2>
-        <Row xs={1} sm={2} md={3} lg={3}>
-          {donationItems.map((item, index) => (
-            <Col key={index} className="mb-4" xs={12} md={6} lg={4}>
-              <Card>
-                <Card.Body>
-                  <Card.Title>{item.name}</Card.Title>
-                  <Card.Text>${item.price}</Card.Text>
-                  <Form>
-                    <Form.Group controlId={`quantity-${index}`}>
-                      <Form.Label>Quantity</Form.Label>
-                      <Form.Control type="number" min="0" value={item.quantity} onChange={(event) => handleQuantityChange(index, event)} />
-                    </Form.Group>
-                  </Form>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-        <hr />
-        <h6>Donation Summary : </h6>
-        <Row>
-          <Col xs={12} md={6}>
-            {donationItems.map((item, index) => (
-              item.quantity > 0 && (
-                <p key={index}>{item.name}: {item.quantity} x ${item.price} = ${item.price * item.quantity}</p>
-              )
-            ))}
-          </Col>
-          <Col xs={12} md={6} className="text-center">
-            <h4>Total Cost</h4>
-            <p className="fs-4">${totalCost}</p>
-            <Button variant="primary" onClick={handleDonateNow}>Donate Now</Button>
-          </Col>
-        </Row>
-      </Container>
-      <Footer/>
-    </div>
-  );
-}
+    <MainContainer>
+    <h1 style={{ color: "#1a2a6c" }}>Disaster Gift Box Donation</h1>
+      <ContentContainer>
+        <h3>Choose a Gift Box to Donate</h3>
+        <p>Each gift box contains essential items that will help disaster survivors get through the toughest times.</p>
+        <GiftBoxContainer>
+          <GiftBoxItem>
+            <GiftBoxImage src= {image} alt="Gift Box 1" />
+            <h4>Food and Water Box</h4>
+            <p>This box contains non-perishable food items and bottled water.</p>
+            <DonationButton>Donate Now</DonationButton>
+          </GiftBoxItem>
+          <GiftBoxItem>
+            <GiftBoxImage src={image} alt="Gift Box 2" />
+            <h4>Clothing Box</h4>
+            <p>This box contains new or gently used clothing items and blankets.</p>
+            <DonationButton>Donate Now</DonationButton>
+            </GiftBoxItem>
+</GiftBoxContainer>
+<FormContainer>
+<h3>Donate Without Money</h3>
+<InputField type="text" placeholder="Your Name" />
+<InputField type="email" placeholder="Your Email" />
+<InputField type="text" placeholder="Your Address" />
+<DonateWithoutMoneyContainer>
+<p>If you don't have money to donate, you can still help by volunteering or spreading awareness about the disaster.</p>
+<p>For more information, visit our website.</p>
+</DonateWithoutMoneyContainer>
+<Button variant="secondary" type="submit">
+<FaDonate /> Donate Without Money
+</Button>
+</FormContainer>
+</ContentContainer>
+</MainContainer>
+<Footer/>
+</div>
+);
+};
 
-export default Donation;
-
-
-
-
-
-
+export default Donation
