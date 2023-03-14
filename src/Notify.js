aa75ed3b55b7e519fddfac0868c5c58becd61e8c
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-15 00:42:29 +0530

    Update Notify.js

--- src/Notify.js
+++ src/Notify.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
@@ -8,6 +8,7 @@ import Navbar from "./components/Nav/Navbar";
 import Footer from "./Footer";
 
 function App() {
+  const [cameraFacingMode, setCameraFacingMode] = useState("user"); // "user" is front camera, "environment" is back camera
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
@@ -50,46 +51,71 @@ function App() {
     }
   };
 
-  runHandpose();
+  useEffect(() => {
+    runHandpose();
+  }, []);
+
+  const handleCameraFlip = () => {
+    setCameraFacingMode((prevMode) =>
+      prevMode === "user" ? "environment" : "user"
+    );
+  };
 
   return (
     <div className="App">
-      <Navbar/>
-      <h1 style={{ color: "#1a2a6c" }}>Hands-free detection for hands in need</h1>
-      <header className="App-header">
+      <Navbar />
+      <h1 style={{ color: "#1a2a6c" }}>
+        Hands-free detection for hands in need
+      </h1>
+      <div className="video-container">
         <Webcam
+          audio={false}
           ref={webcamRef}
           style={{
             position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
             left: 0,
             right: 0,
             textAlign: "center",
             zindex: 9,
-            width: 640,
-            height: 480,
+            width: "100%",
+            height: "auto",
+            maxWidth: "640px",
+            maxHeight: "480px",
+          }}
+          videoConstraints={{
+            facingMode: cameraFacingMode,
           }}
         />
-
         <canvas
           ref={canvasRef}
           style={{
             position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
             left: 0,
             right: 0,
             textAlign: "center",
             zindex: 9,
-            width: 640,
-            height: 480,
+            width: "100%",
+            height: "auto",
+            maxWidth: "640px",
+            maxHeight: "480px",
           }}
         />
-      </header>
-      <Footer/>
+        <button
+          onClick={handleCameraFlip}
+          style={{
+            position: "absolute",
+            bottom: 100,
+            left: "50%",
+            transform: "translateX(-50%)",
+            zIndex: 10,
+          }}
+        >
+          Flip Camera
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default App
