0b33a649797ee8b7d5228e71acfd7aa23b2e56ee
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-03-25 11:26:01 +0530

    Update Donation.js

--- src/Donation.js
+++ src/Donation.js
@@ -102,13 +102,13 @@ const DisasterGiftBoxPage = () => {
               <DonationButton>Donate Now</DonationButton>
             </GiftBoxItem>
             <GiftBoxItem>
-               <GiftBoxImage src={image} alt="Gift Box 2" />
-               <h4>Clothing Box</h4>
-               <p>This box contains new or gently used clothing items and blankets.</p>
-               <DonationButton>Donate Now</DonationButton>
+              <GiftBoxImage src={image} alt="Gift Box 2" />
+              <h4>Clothing Box</h4>
+              <p>This box contains new or gently used clothing items and blankets.</p>
+              <DonationButton>Donate Now</DonationButton>
             </GiftBoxItem>
           </GiftBoxContainer>
-        <FormContainer>
+          <FormContainer>
             <h3>Donate Without Money</h3>
             <InputField type="text" placeholder="Your Name" />
             <InputField type="email" placeholder="Your Email" />
@@ -116,8 +116,10 @@ const DisasterGiftBoxPage = () => {
             <DonateWithoutMoneyContainer>
               <p>If you don't have money to donate, you can still help by volunteering or spreading awareness about the disaster.</p>
               <p>For more information, visit our website.</p>
+              <Button variant="secondary" type="submit">
+              <FaDonate /> Donate Without Money</Button>
             </DonateWithoutMoneyContainer>
-        </FormContainer>
+          </FormContainer>
       </ContentContainer>
     </MainContainer>
     <Footer/>
